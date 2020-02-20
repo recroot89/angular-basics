764b914dac6cb4379f2cf72c4f7647e19b5cb7e0
recroot89 <recroot89@gmail.com>
2020-02-20 17:57:40 +0500

    add posts data to html component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,22 @@
 import { Component } from "@angular/core";
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {}
+export class AppComponent {
+  posts: Post[] = [
+    {
+      title: "I want to learn Angular components",
+      text: "I am still learning",
+      id: 1
+    },
+    { title: "Next lesson", text: "Directives and pipes", id: 2 }
+  ];
+}
