f29baa6456ff2655f1e452bc598d472246b96d90
recroot89 <recroot89@gmail.com>
2020-03-10 16:27:44 +0500

    prepare blog app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,8 @@
-import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormControl, Validators, FormArray } from "@angular/forms";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
-export class AppComponent implements OnInit {
-  form: FormGroup;
-
-  ngOnInit() {
-    this.form = new FormGroup({
-      email: new FormControl("", [Validators.email, Validators.required]),
-      password: new FormControl(null, [
-        Validators.required,
-        Validators.minLength(6)
-      ]),
-      address: new FormGroup({
-        country: new FormControl("ru"),
-        city: new FormControl("", Validators.required)
-      }),
-      skills: new FormArray([])
-    });
-  }
-
-  submit() {
-    console.log("Form submitted", this.form);
-  }
-
-  setCapital() {
-    const cityMap = {
-      ru: "Москва",
-      ua: "Киев",
-      by: "Минск"
-    };
-
-    const city = cityMap[this.form.get("address").get("country").value];
-
-    this.form.patchValue({ address: { city } });
-  }
-
-  addSkill() {
-    const control = new FormControl("", Validators.required);
-    (this.form.get("skills") as FormArray).push(control);
-  }
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppComponent } from "./app.component";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PostPageComponent } from './post-page/post-page.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
+  declarations: [
+    AppComponent,
+    MainLayoutComponent,
+    HomePageComponent,
+    PostPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
